3dec62d3d5ff72aa780d93a45b861d0b3e5cd016
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2024-01-24 14:20:37 +0530

    first commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import * as images from "../image";
+
+    interface Quiz {
+        id:number;
+        name: string;
+        imageUrl: keyof typeof images;
+        answer: string
+        questions: {id: number; text: string; options: {id: number; text: string; label: string}[]}[]
+    }
+
+    export let quiz: Quiz;
+</script>
+
+<button class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2" on:click={() => goto(`/quiz/${quiz.id}`)}>
+    <img class="h-44" src={images[quiz.imageUrl]} alt="" />
+    <div class="flex items-center justify-between py-3 px-2">
+        <h1 class="font-bold">{quiz.name}</h1>
+        <p>{quiz.questions.length} Questions</p>
+    </div>
+</button>
---
+++ src/image.ts
@@ -0,0 +1,5 @@
+export const chemistry =
+  "https://www.thoughtco.com/thmb/EpsKarnLz-v0VXY0KKnS_fTX5-U=/2121x1414/filters:fill(auto,1)/GettyImages-545286316-433dd345105e4c6ebe4cdd8d2317fdaa.jpg";
+
+export const biology =
+  "https://www.visiblebody.com/hubfs/learn/billboard/visible-body-3d-biology-learn-social.png";
